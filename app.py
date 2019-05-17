e1bbdfe9f9c093b8027cd85232d62a17a0df5f81
Ganesh Crooks <gcroo001@gmail.com>
2019-05-16 21:16:44 -0400

    added templates directory

--- app.py
+++ app.py
@@ -6,3 +6,23 @@ import pandas as pd
 
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    #Return to Home
+    return('index.html')
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run()
