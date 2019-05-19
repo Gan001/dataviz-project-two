01b0403b4f3dbba5053c869d64d4b951067b0bd5
Ganesh Crooks <gcroo001@gmail.com>
2019-05-19 02:18:44 -0400

    moved files to different directories

--- app.py
+++ app.py
@@ -8,11 +8,19 @@ from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+#Connect to local database
+rds_connection_string = f"root:{password}@localhost/services_311_db"
+engine = create_engine(f'mysql://{rds_connection_string}')
 
 @app.route("/")
 def index():
     #Return to Home
-    return('index.html')
+    return render_template('index.html')
+
+@app.route("/services")
+def service():
+    service = pd.read_sql_query('select * from requests_311', con=engine).head()
+    return jsonify(list(service.columns))
 
 
 
